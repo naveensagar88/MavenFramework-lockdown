770991f7d1a879499d94e522d4377b5c254f2190
Naveen <navi.sagar.88@gmail.com>
2020-05-09 19:31:59 +0530

    Maven commit

---
+++ Mavenjava/src/test/java/qaclickacademy/Apptest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class Apptest {
+	
+	
+	@Test
+	public void Nativeappandroid()
+	{
+		System.out.println("Nativeappandroid");
+	}
+	
+	@Test
+	public void IOSapp()
+	{
+		System.out.println("IOSapp");
+	}
+
+}
